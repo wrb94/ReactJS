1a61f7cb75a8c287731c9ad591df78aeaa66aa77
wrb94 <209150@student.pwr.edu.pl>
2017-01-10 20:29:09 +0100

    Changes

--- src/Demo1/Message.js
+++ src/Demo1/Message.js
@@ -1,11 +1,19 @@
 import React from 'react'
 
-const Message = React.createClass({
-  render () {
-    return (
-      <h1>Message!!!</h1>
-    )
-  }
-})
+const {string} = React.PropTypes
+
+const Message = (props) => <h1>{props.message}</h1>
+
+// const Message = React.createClass({
+//   propTypes: {
+//     message: React.PropTypes.string,
+//     value: React.PropTypes.number
+//   },
+//   render() {
+//     return (
+//       <h1>{this.props.message}</h1>
+//     )
+//   }
+// })
 
 export default Message
--- src/Demo1/MyFirstComponent.js
+++ src/Demo1/MyFirstComponent.js
@@ -2,10 +2,34 @@ import React from 'react'
 import Message from './Message'
 
 const MyFirstComponent = React.createClass({
-  render () { // one DOM element on return
+  getInitialState() {
+    return {
+      value: 0,
+      disabled: false
+    }
+  },
+  countUp(i) {
+    console.log('click')
+    this.setState({
+      value: this.state.value + i
+    }) // TODO function handler
+  },
+  countDown(i) {
+    console.log('down')
+    this.setState({
+      value: this.state.value - i
+    })
+  },
+  // this.props  this.state.value || this.setState({value:1})
+  render() { // one DOM element on return
+
     return (
       <div>
-        <Message />
+        <Message message='Hello1' />
+        <Message message={this.state.value} />
+        <button onClick={() => this.countUp(1)}>Count Up!</button>
+        <br />
+        <button onClick={this.countDown.bind(this, -1)} disabled={false}>Count Down!</button>
       </div>
     )
   }
