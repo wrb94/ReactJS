84d8e659e1753eb813446290878174898631b20c
wrb94 <209150@student.pwr.edu.pl>
2017-02-15 19:15:24 +0100

    Added Tasks.js

--- lab2/src/NozbeApp.js
+++ lab2/src/NozbeApp.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Route, Router, hashHistory } from 'react-router'
+import { Route, Router, browserHistory } from 'react-router'
 import Calendar from './Calendar.js'
+import Tasks from './Tasks.js'
 
 render(
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path='/' component={Calendar} />
     <Route path='/tasks' component={Tasks} />
   </Router>,
