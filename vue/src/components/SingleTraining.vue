f584aca081af463a19fc97c976bb3d535ecabd81
wrb94 <209150@student.pwr.edu.pl>
2017-04-11 19:31:00 +0200

    More components

---
+++ vue/src/components/SingleTraining.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="col-xs-6">
+              <div class="panel panel-primary">
+                <div class="panel-heading">{{training.name}}</div>
+                <div class="panel-body">
+                  <p>{{training.description}}</p>
+                  <button class="btn btn-primary center-block" @click="chooseTraining">Wybierz</button>
+                </div>
+              </div>
+            </div>
+</template>
+
+<script>
+export default {
+  props: ['training'],
+  methods: {
+    chooseTraining(){
+      this.$emit('trainingWasChosen', this.training)
+    }
+  }
+}
+</script>
